32c68f2a0b6482c32cd0339bf5cdad6668d01775
Tim Beatty <timb@patriot1tech.com>
2021-08-13 09:33:12 -0300

    Enable configuration of CUDA extension arch options through TORCH_CUDA_ARCH_LIST by removing explict arch flags

--- setup.py
+++ setup.py
@@ -12,12 +12,8 @@ setup(
         extra_compile_args={
             'cxx': ['-std=c++14', '-O2', '-Wall'],
             'nvcc': [
-                '-std=c++14', '--expt-extended-lambda', '--use_fast_math', '-Xcompiler', '-Wall,-fno-gnu-unique',
-                '-gencode=arch=compute_60,code=sm_60', '-gencode=arch=compute_61,code=sm_61',
-                '-gencode=arch=compute_70,code=sm_70', '-gencode=arch=compute_72,code=sm_72',
-                '-gencode=arch=compute_75,code=sm_75', '-gencode=arch=compute_80,code=sm_80',
-                '-gencode=arch=compute_86,code=sm_86', '-gencode=arch=compute_86,code=compute_86'
-            ],
+                '-std=c++14', '--expt-extended-lambda', '--use_fast_math', '-Xcompiler', '-Wall,-fno-gnu-unique'
+           ],
         },
         libraries=['nvinfer', 'nvinfer_plugin', 'nvonnxparser', 'opencv_core', 'opencv_imgproc', 'opencv_highgui', 'opencv_imgcodecs'])
     ],
